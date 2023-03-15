2ab4f1fd391de3e4d05d217a1b14f7abc0de1039
Ebube Okoli <ebubeokoli@rocketmail.com>
2023-03-15 01:02:53 +0100

    Included stake and fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
-pub mod wordchain;
+mod wordchain;
 
 
--- src/wordchain.rs
+++ src/wordchain.rs
@@ -1,26 +1,31 @@
 
+use std::collections::HashMap;
+
 use near_contract_standards::fungible_token::metadata::{
     FungibleTokenMetadata, FungibleTokenMetadataProvider, FT_METADATA_SPEC,
 };
-use near_contract_standards::fungible_token::FungibleToken;
+use near_contract_standards::fungible_token::receiver::FungibleTokenReceiver;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LazyOption, LookupMap, Vector};
 use near_sdk::json_types::U128;
+use near_sdk::serde::Serialize;
 use near_sdk::{env, log, near_bindgen, AccountId, Balance, PanicOnDefault, BorshStorageKey, PromiseOrValue, ext_contract, require};
 
 pub const DAY_TO_MS: u64 = 86400000;
 
 #[derive(Eq, PartialEq)]
-#[derive(Clone, BorshDeserialize, BorshSerialize)]
-enum TournamentType {
+#[derive(Clone, BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub enum TournamentType {
     PUBLIC,
     PRIVATE,
     COUNTRY_BASED,
 }
 
 #[derive(Eq, PartialEq)]
-#[derive(Clone, BorshDeserialize, BorshSerialize)]
-enum TournamentState {
+#[derive(Clone, BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub enum TournamentState {
     PENDING_VOLUME,
     ACTIVE,
     DELETED,
@@ -28,20 +33,23 @@ enum TournamentState {
 }
 
 #[derive(Eq, PartialEq)]
-#[derive(Clone, BorshDeserialize, BorshSerialize)]
+#[derive(Clone, BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
 enum PlayerState {
     ACTIVE,
     BLACKLISTED,
 }
 
-#[derive(Clone, BorshDeserialize, BorshSerialize)]
+#[derive(Clone, BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
 struct GameType {
     identifier: String,
     max_score: u64,
 }
 
 #[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
-#[derive(Clone, BorshDeserialize, BorshSerialize)]
+#[derive(Clone, BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
 struct TournamentPlayer {
     id: U128,
     account_id: AccountId,
@@ -50,7 +58,8 @@ struct TournamentPlayer {
     join_date: u64
 }
 
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
 struct Tournament {
     id: U128,
     name: String,
@@ -61,34 +70,62 @@ struct Tournament {
     created_at: u64,
     total_stake: Balance,
     country: String,
+    ft_address: AccountId,
     tournament_deadline: u64,
     tournament_type: TournamentType,
     status: TournamentState,
-    players: Vector<TournamentPlayer>
+    players: Vec<TournamentPlayer>
 }
 
 #[ext_contract(ext_token_contract)]
-pub trait StableCoin {
-    fn internal_transfer(&self, from: &AccountId, to: &AccountId, amount: Balance, );
+trait StableCoin {
+    fn ft_transfer(&self, to: &AccountId, amount: Balance, memo: Option<String>);
 
 }
 
+#[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 struct Wordchain {
     paused: bool,
     owner: AccountId,
     pending_owner: AccountId,
-    token_address: String,
     min_tournament_players: u8,
-    stake_payouts: Balance,
+    stake_payouts: LookupMap<AccountId, Balance>,
     percentage_stake_commission: u64,
     game_types: Vector<GameType>,
     tournaments_to_players: LookupMap<U128, Vector<AccountId>>,
     tournaments: Vector<Tournament>,
     supported_countries: Vector<String>,
+    stakes: LookupMap<AccountId, LookupMap<AccountId, Balance>>,
+
 }
 
+#[near_bindgen]
 impl Wordchain {
+
+    #[init]
+    pub fn new(percentage_stake_commission: u64, countries: String, min_tournament_players: u8) -> Self {
+
+        let countries_split = countries.split("|").collect::<Vec<&str>>();
+
+        let mut supported_countries = Vector::new(b"c");
+
+        countries_split.into_iter().for_each(|cont| supported_countries.push(&cont.to_string()));
+
+        Self {
+            paused: bool::default(),
+            owner: env::signer_account_id(),
+            pending_owner: env::current_account_id(),
+            min_tournament_players,
+            stake_payouts: LookupMap::new(b"d"),
+            percentage_stake_commission,
+            game_types: Vector::new(b"g"),
+            tournaments_to_players: LookupMap::new(b"p"),
+            tournaments: Vector::new(b"t"),
+            supported_countries,
+            stakes: LookupMap::new(b"s")
+        }
+    }
 
     /// .
     pub fn create_tournament(&mut self, 
@@ -98,6 +135,7 @@ impl Wordchain {
         form: TournamentType, 
         interval: u64, 
         minimum_stake: U128, 
+        ft_address: AccountId,
         country: Option<String>) -> Tournament {
 
         match form {
@@ -112,39 +150,55 @@ impl Wordchain {
         require!(self.get_tournament_by_name(name.clone()).is_none(), "Tournament with provided name already exists");
         require!(self.get_gametypes(Some(game_type_id.clone())).len() > 0, "No tournament with provided game type");
 
-        let tournament_id = U128::from((self.tournaments.len() as u128) + 1);
+        if let Some(stakings) = Some(self.stakes.get(&env::predecessor_account_id())) {
+            let mut stakes = stakings.unwrap();
+            let ft_stake = stakes.get(&ft_address).unwrap_or_else(|| env::panic_str("No stake made"));
 
-        let mut players_ = Vector::new(b"p");
+            if ft_stake < minimum_stake.0 {
+                env::panic_str("You must have staked at least the minimum stake before creating tournament");
+            } else {
 
-        if env::predecessor_account_id() != self.owner {
-            players_.push(&TournamentPlayer {
-                id: U128::from(1),
-                account_id: env::predecessor_account_id(),
-                stake_amount: minimum_stake.0,
-                score: 0,
-                join_date: env::block_timestamp_ms(),
-            });
+                let tournament_id = U128::from((self.tournaments.len() as u128) + 1);
+
+                let mut players_ = Vec::new();
+
+                if env::predecessor_account_id() != self.owner {
+                    players_.push(TournamentPlayer {
+                        id: U128::from(1),
+                        account_id: env::predecessor_account_id(),
+                        stake_amount: minimum_stake.0,
+                        score: 0,
+                        join_date: env::block_timestamp_ms(),
+                    });
+                }
+
+                let tournament = Tournament {
+                    id: tournament_id,
+                    name,
+                    tournament_key,
+                    game_type_id,
+                    owner: env::predecessor_account_id(),
+                    minimum_stake: minimum_stake.0,
+                    total_stake: 0,
+                    country: country.unwrap_or_default(),
+                    ft_address,
+                    created_at: env::block_timestamp_ms(),
+                    tournament_deadline: env::block_timestamp_ms() + (interval * DAY_TO_MS),
+                    tournament_type: form,
+                    status: TournamentState::PENDING_VOLUME,
+                    players: players_
+                };
+
+                self.tournaments.push(&tournament);
+
+                stakes.insert(&tournament.ft_address, &0);
+                self.stakes.insert(&env::predecessor_account_id(), &stakes);
+
+                tournament
+            }
+        } else {
+            env::panic_str("No stake made");
         }
-
-        let tournament = Tournament {
-            id: tournament_id,
-            name,
-            tournament_key,
-            game_type_id,
-            owner: env::predecessor_account_id(),
-            minimum_stake: minimum_stake.0,
-            total_stake: 0,
-            country: country.unwrap_or_default(),
-            created_at: env::block_timestamp_ms(),
-            tournament_deadline: env::block_timestamp_ms() + (interval * DAY_TO_MS),
-            tournament_type: form,
-            status: TournamentState::PENDING_VOLUME,
-            players: players_
-        };
-
-        self.tournaments.push(&tournament);
-
-        tournament
 
     }
 
@@ -152,7 +206,6 @@ impl Wordchain {
     /// .
     pub fn join_tournament(&mut self,
         tournament_id: U128,
-        stake_amount: U128,
         country: String,
         tournament_key: Option<String>,
     ) {
@@ -170,23 +223,39 @@ impl Wordchain {
             _ => {}
         }
 
-        let number_of_players = (tournament.players.len() as u128) + 1;
+        if let Some(stakings) = Some(self.stakes.get(&env::predecessor_account_id())) {
 
-        let player_id = U128::from(number_of_players.clone());
+            let mut stakes = stakings.unwrap();
+            let ft_stake = stakes.get(&tournament.ft_address).unwrap_or_else(|| env::panic_str("No stake made"));
 
-        tournament.players.push(&TournamentPlayer {
-            id: player_id,
-            account_id: env::predecessor_account_id(),
-            stake_amount: stake_amount.0,
-            score: 0,
-            join_date: env::block_timestamp_ms(),
-        });
+            if ft_stake < tournament.minimum_stake {
+                env::panic_str("You must have staked at least the minimum stake before creating tournament");
+            } else {
+                let number_of_players = (tournament.players.len() as u128) + 1;
 
-        if number_of_players >= self.min_tournament_players as u128 {
-            tournament.status = TournamentState::ACTIVE;
+                let player_id = U128::from(number_of_players.clone());
+
+                tournament.players.push(TournamentPlayer {
+                    id: player_id,
+                    account_id: env::predecessor_account_id(),
+                    stake_amount: ft_stake,
+                    score: 0,
+                    join_date: env::block_timestamp_ms(),
+                });
+
+                if number_of_players >= self.min_tournament_players as u128 {
+                    tournament.status = TournamentState::ACTIVE;
+                }
+
+                self.tournaments.replace(tournament_id.0 as u64, &tournament);
+                stakes.insert(&tournament.ft_address, &0);
+                self.stakes.insert(&env::predecessor_account_id(), &stakes);
+            }
+
+        } else {
+            env::panic_str("No stake made");
         }
-
-        self.tournaments.replace(tournament_id.0 as u64, &tournament);
+  
         
     }
 
@@ -201,7 +270,7 @@ impl Wordchain {
         let gametype = self.get_gametypes(Some(tournament.game_type_id.clone()));
         require!(score as u64 <= gametype[0].max_score, "Score exceeds threshold for game");
 
-        tournament.players.iter().for_each(|mut player| {
+        tournament.players.clone().into_iter().for_each(|mut player| {
             if player.account_id == env::predecessor_account_id() {
                 player.score += score as u64;
             }
@@ -221,14 +290,24 @@ impl Wordchain {
         require!(tournament.tournament_deadline <= env::block_timestamp_ms(), "Tournament owner can not join the tournament");
         require!(tournament.status != TournamentState::CLOSED, "Tournament is already closed");
 
+        let mut stake_payout_for_ft = self.stake_payouts.get(&tournament.ft_address).unwrap_or_default();
+
         match tournament.status {
             TournamentState::PENDING_VOLUME => {
                 tournament.players.iter().for_each(|player| {
                     
                     if player.account_id != tournament.owner {
+                        ext_token_contract::ext(tournament.ft_address.clone())
+                        .ft_transfer(
+                            &player.account_id,
+                            player.stake_amount,
+                            None
+                        );
 
                     } else {
-                        self.stake_payouts += player.stake_amount;
+
+                        stake_payout_for_ft += player.stake_amount;
+                        self.stake_payouts.insert(&tournament.ft_address, &stake_payout_for_ft);
                     }
                 });
 
@@ -238,11 +317,12 @@ impl Wordchain {
             TournamentState::DELETED => {},
             _ => {
                 let commission = (self.percentage_stake_commission as u128 * tournament.total_stake) / 10000;
-                self.stake_payouts += commission;
+                stake_payout_for_ft += commission;
+                self.stake_payouts.insert(&tournament.ft_address, &stake_payout_for_ft);
 
                 let pay_value = tournament.total_stake - commission;
 
-                let mut players = tournament.players.iter().map(|player| player).collect::<Vec<TournamentPlayer>>();
+                let mut players = tournament.players.clone().into_iter().map(|player| player).collect::<Vec<TournamentPlayer>>();
 
                 players.sort_by(|a, b| b.score.cmp(&a.score));
 
@@ -251,7 +331,12 @@ impl Wordchain {
                 for i in 1..prizes.len() {
                     let val_to_pay = prizes[i] as u128 * pay_value;
 
-
+                    ext_token_contract::ext(tournament.ft_address.clone())
+                        .ft_transfer(
+                            &players[i].account_id,
+                            val_to_pay,
+                            None
+                        );
                 }
                 tournament.status = TournamentState::CLOSED;
                 self.tournaments.replace(tournament_id.0 as u64, &tournament);
@@ -356,6 +441,26 @@ impl Wordchain {
         res
     }
     
+}
+
+impl FungibleTokenReceiver for Wordchain {
+
+    fn ft_on_transfer(&mut self,sender_id:AccountId,amount:U128,msg:String,) -> PromiseOrValue<U128> {
+        let mut stakings = self.stakes.get(&sender_id).unwrap_or(LookupMap::new(b"g"));
+
+        let mut stake_balance = stakings.get(&env::predecessor_account_id()).unwrap_or_default();
+
+        stake_balance += amount.0;
+        stakings.insert(&env::predecessor_account_id(), &stake_balance);
+        self.stakes.insert(&sender_id, &stakings);
+
+        PromiseOrValue::Value(U128::from(0))
+
+        // let tournament_staked_for = msg.replace("tournament ", "");
+
+        // let id = U128::try_from(tournament_staked_for);
+
+    }
 }
 
 
